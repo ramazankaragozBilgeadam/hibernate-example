e64e1518a54565e50722dba4019eb7e56638aaba
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-05-12 13:22:33 +0300

    add customerınformation ders 30

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
+import dao.CustomerDAO;
 import dao.MenuDAO;
 import dao.ProductDAO;
-import entity.BaseEntity;
-import entity.Menu;
-import entity.Product;
+import entity.*;
 import util.HibernateUtil;
 
 import java.util.ArrayList;
@@ -14,20 +13,71 @@ public class Main {
 
     public static void main(final String[] args) throws Exception {
 
-      //onMenuKaydet("Menu 1");
+        //onMenuKaydet("Menu 1");
+        //displayProductList(1L);
 
-      displayProductList(1L);
+        //displayProductListWithHql();
+        //displayProductListWithCriteria();
+
 
     }
 
 
-    public static void displayProductList(Long menuId){
+    public static void customerKaydet(){
 
-        ProductDAO productDAO=new ProductDAO();
+        CustomerDAO customerDAO=new CustomerDAO();
+
+        CustomerInformation customerInformation=new CustomerInformation();
+        customerInformation.setCity("Ankara");
+        customerInformation.setPhoneNumber("5318245656");
+        customerInformation.setStreet("Sümbül");
+        customerInformation.setDetailAddress("Parkın Karşısı");
+        customerInformation.setCreatedBy("Ramazan");
+
+        CustomerOrder customerOrder=new CustomerOrder();
+        customerOrder.setCreatedBy("Ramazan");
+
+
+
+    }
+
+    public static Double getOrderTotal(){
+
+        Double total=0.0;
+
+
+        return total;
+    }
+
+
+    public static void displayProductListWithHql() {
+        ProductDAO productDAO = new ProductDAO();
+
+        try {
+            productDAO.findAllWithHql().stream().forEach(System.out::println);
+        } catch (NullPointerException ex) {
+            System.out.println("Products are not found.");
+        }
+    }
+
+    public static void displayProductListWithCriteria() {
+        ProductDAO productDAO = new ProductDAO();
+
+        try {
+            productDAO.findAllWithCriteria().stream().forEach(System.out::println);
+        } catch (NullPointerException ex) {
+            System.out.println("Products are not found.");
+        }
+    }
+
+
+    public static void displayProductList(Long menuId) {
+
+        ProductDAO productDAO = new ProductDAO();
 
         try {
             productDAO.findAllByMenuId(menuId).stream().forEach(System.out::println);
-        }catch (NullPointerException ex){
+        } catch (NullPointerException ex) {
             System.out.println("Products are not found.");
         }
 
@@ -35,24 +85,24 @@ public class Main {
     }
 
 
-    public static void onMenuKaydet(String menuName){
+    public static void onMenuKaydet(String menuName) {
 
-        MenuDAO menuDAO=new MenuDAO();
+        MenuDAO menuDAO = new MenuDAO();
 
-        Product product1=new Product();
+        Product product1 = new Product();
         product1.setProductName("Cortado");
         product1.setPrice(12.00);
         product1.setCreatedBy("Ramazan");
 
-        Product product2=new Product("Latte",15.00);
+        Product product2 = new Product("Latte", 15.00);
         product2.setCreatedBy("Ali");
 
-        List<Product> productList=new ArrayList<>();
+        List<Product> productList = new ArrayList<>();
         productList.add(product1);
         productList.add(product2);
 
 
-        Menu menu=new Menu();
+        Menu menu = new Menu();
         menu.setMenuName(menuName);
         menu.setProductList(productList);
         menu.setCreatedBy("Ramazan");
--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -5,6 +5,9 @@ import org.hibernate.HibernateException;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class ProductDAO extends BaseDAO {
@@ -46,6 +49,56 @@ public class ProductDAO extends BaseDAO {
             productQuery.setParameter("menuId",menuId);
 
             productList=productQuery.getResultList();
+
+        }catch (HibernateException ex){
+            ex.printStackTrace();
+            transaction.rollback();
+        }finally {
+            transaction.commit();
+        }
+
+        return productList;
+    }
+
+
+    public List<Product> findAllWithHql(){
+
+        Transaction transaction=null;
+        List<Product> productList=null;
+        try{
+
+            transaction=getSession().beginTransaction();
+
+            productList=getSession()
+                    .createQuery("from Product",Product.class)
+                    .getResultList();
+
+        }catch (HibernateException ex){
+            ex.printStackTrace();
+            transaction.rollback();
+        }finally {
+            transaction.commit();
+        }
+
+        return productList;
+    }
+
+    public List<Product> findAllWithCriteria(){
+
+        Transaction transaction=null;
+        List<Product> productList=null;
+
+        try{
+            transaction=getSession().beginTransaction();
+
+            CriteriaBuilder cb=getSession().getCriteriaBuilder();
+            CriteriaQuery<Product> criteriaQuery=cb.createQuery(Product.class);
+            Root<Product> root=criteriaQuery.from(Product.class);
+
+            criteriaQuery.select(root).where(cb.lessThan(root.get("price"),15.00));
+
+            productList=getSession().createQuery(criteriaQuery).getResultList();
+
 
         }catch (HibernateException ex){
             ex.printStackTrace();
